5eb4f791ef37a77f7723505fe0cdf3037e69920d
GrumpycatInHeaven <53279534+GrumpycatInHeaven@users.noreply.github.com>
2019-10-20 14:39:19 +0300

    Create d_3.js

---
+++ d_3.js
@@ -0,0 +1,23 @@
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Фактор', ''],
+          ['Некоторые инфекционные и паразитарные болезни',     0.030233],
+          ['Новообразования',      0.19866],
+          ['Болезни системы кровообращения',  0.507258],
+          ['Болезни органов дыхания', 0.053079],
+          ['Болезни органов пищеварения',    0.063729],
+          ['Внешние причины', 0.147041]
+        ]);
+
+        var options = {
+          title: 'Мужчины'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
